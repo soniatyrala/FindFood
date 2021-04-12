d25fc4aa72140b50f3a290790fcc5fc082c6cbf2
styrala <tyrala.sonia@gmail.com>
2021-04-12 11:35:15 +0200

    set custom marker on google maps

--- app/src/main/java/com/styrala/findfood/MapsActivity.kt
+++ app/src/main/java/com/styrala/findfood/MapsActivity.kt
@@ -26,9 +26,9 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        // Add a marker in Boronów and move the camera
+        val boronow = LatLng(50.675, 18.906)
+        mMap.addMarker(MarkerOptions().position(boronow).title("Marker in Boronów"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(boronow))
     }
 }

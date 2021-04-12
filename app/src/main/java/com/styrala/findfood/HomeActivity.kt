edcde5d27ff84c2708cb5224958abd01467edc47
styrala <tyrala.sonia@gmail.com>
2021-04-12 10:41:24 +0200

    action button food map

--- app/src/main/java/com/styrala/findfood/HomeActivity.kt
+++ app/src/main/java/com/styrala/findfood/HomeActivity.kt
@@ -1,7 +1,10 @@
 package com.styrala.findfood
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+
 
 class HomeActivity : AppCompatActivity() {
 
@@ -9,4 +12,10 @@ class HomeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.home_activity)
     }
+
+    fun openFoodMaps(view: View?) {
+        val intent = Intent(this@HomeActivity, MapsActivity::class.java)
+        startActivity(intent)
+    }
+
 }

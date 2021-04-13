019dcaa588c123be327ab087936b71aa9afe2a91
styrala <tyrala.sonia@gmail.com>
2021-04-13 18:31:45 +0200

    set current location

--- app/src/main/java/com/styrala/findfood/MapsActivity.kt
+++ app/src/main/java/com/styrala/findfood/MapsActivity.kt
@@ -1,34 +1,55 @@
 package com.styrala.findfood
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
-
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
+    private lateinit var mapFragment: SupportMapFragment
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+    private var latitude = 0.0
+    private var longitude = 0.0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_maps)
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        val mapFragment = supportFragmentManager
-                .findFragmentById(R.id.map) as SupportMapFragment
-        mapFragment.getMapAsync(this)
+        mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        getCurrentLocation()
     }
 
-    override fun onMapReady(googleMap: GoogleMap) {
-        mMap = googleMap
+    override fun onMapReady(googleMap: GoogleMap?) {
+        mMap = googleMap!!
+        val location = LatLng(this.latitude, this.longitude)
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(location, 15f))
+    }
 
-        // Add a marker in Boronów and move the camera
-        val boronow = LatLng(50.675, 18.906)
-        mMap.addMarker(MarkerOptions().position(boronow).title("Marker in Boronów"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(boronow))
+    private fun getCurrentLocation() {
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+            == PackageManager.PERMISSION_GRANTED) {
+            fusedLocationProviderClient.lastLocation.addOnSuccessListener { location ->
+                latitude = location?.latitude!!
+                longitude = location.longitude
+                mapFragment.getMapAsync(this)
+            }
+        } else {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                44)
+            getCurrentLocation()
+        }
     }
 }
+

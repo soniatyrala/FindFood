e9612f57fb9084aa4d60bba75628eb09ef2b13e9
styrala <tyrala.sonia@gmail.com>
2021-04-15 18:29:27 +0200

    map json data to model

---
+++ app/src/main/java/com/styrala/findfood/service/IGoogleAPIService.kt
@@ -0,0 +1,12 @@
+package com.styrala.findfood.service
+
+import com.styrala.findfood.model.Places
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Url
+
+interface IGoogleAPIService {
+
+    @GET
+    fun getNearbyPlaces(@Url url: String): Call<Places>
+}

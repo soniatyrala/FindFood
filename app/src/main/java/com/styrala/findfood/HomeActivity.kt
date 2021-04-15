7dc38642bf3a099bc4bd82f7c1161948a35b6cc2
styrala <tyrala.sonia@gmail.com>
2021-04-15 23:32:31 +0200

    added visited activity

--- app/src/main/java/com/styrala/findfood/HomeActivity.kt
+++ app/src/main/java/com/styrala/findfood/HomeActivity.kt
@@ -3,6 +3,7 @@ package com.styrala.findfood
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 
@@ -11,11 +12,17 @@ class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.home_activity)
-    }
 
-    fun openFoodMaps(view: View?) {
-        val intent = Intent(this@HomeActivity, MapsActivity::class.java)
-        startActivity(intent)
-    }
+        val buttonMap = findViewById<Button>(R.id.buttonMap)
+        buttonMap.setOnClickListener{
+            val intent = Intent(this, MapsActivity::class.java)
+            startActivity(intent)
+        }
 
+        val buttonVisited = findViewById<Button>(R.id.buttonHistory)
+        buttonVisited.setOnClickListener{
+            val intent = Intent(this, VisitedActivity::class.java)
+            startActivity(intent)
+        }
+    }
 }

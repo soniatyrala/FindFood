ab6cf2b32368675f0fc410e9d1622b770bda9bf2
sonia <tyrala.sonia@gmail.com>
2021-05-12 21:48:21 +0200

    refactor

--- app/src/main/java/com/styrala/findfood/MapsActivity.kt
+++ app/src/main/java/com/styrala/findfood/MapsActivity.kt
@@ -3,7 +3,6 @@ package com.styrala.findfood
 import android.Manifest
 import android.content.pm.PackageManager
 import android.os.Bundle
-import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
@@ -14,8 +13,11 @@ import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
+import com.styrala.findfood.common.Common.MAPS_URL
+import com.styrala.findfood.common.Common.RESTAURANT_TYPE
+import com.styrala.findfood.common.Common.getClient
+import com.styrala.findfood.common.Common.getUrl
 import com.styrala.findfood.model.Places
 import com.styrala.findfood.model.Results
 import com.styrala.findfood.service.BitmapDescriptorService
@@ -23,20 +25,15 @@ import com.styrala.findfood.service.IGoogleAPIService
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
     private lateinit var mapFragment: SupportMapFragment
     private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
-    private lateinit var mMarker: Marker
     private lateinit var mService: IGoogleAPIService
     private var latitude = 0.0
     private var longitude = 0.0
-    private val MAPS_URL = "https://maps.googleapis.com/"
-    private val RESTAURANT_TYPE = "restaurant"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -51,6 +48,9 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         mMap = googleMap!!
         mMap.mapType = GoogleMap.MAP_TYPE_TERRAIN
         mMap.clear()
+        mMap.uiSettings.isCompassEnabled = true
+        mMap.uiSettings.isZoomControlsEnabled = true
+        mMap.uiSettings.isMyLocationButtonEnabled = true
         setCurrentLocationOnMap()
     }
 
@@ -61,43 +61,18 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
                 this.latitude = location?.latitude!!
                 this.longitude = location.longitude
                 val lastLocation = LatLng(latitude, longitude)
-                val markerOptions = MarkerOptions()
-                    .position(lastLocation)
-                    .title("I'm here!")
-                    .icon(BitmapDescriptorService.bitmapFromVector(applicationContext, R.drawable.ic_baseline_person_pin_circle_24))
-                mMarker = mMap.addMarker(markerOptions)
+                mMap.isMyLocationEnabled = true
                 getNearByPlaceType(RESTAURANT_TYPE)
                 mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(lastLocation, 15f))
             }
         } else {
             ActivityCompat.requestPermissions(
-                this,
-                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
-                44)
+                this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), 44)
             setCurrentLocationOnMap()
         }
     }
 
-    private fun getUrl(latitude: Double, longitude: Double, placeType: String): String? {
-        val googlePlacesUrl =
-            StringBuilder("https://maps.googleapis.com/maps/api/place/nearbysearch/json?")
-        googlePlacesUrl.append("location=$latitude,$longitude")
-        googlePlacesUrl.append("&radius=1000")
-        googlePlacesUrl.append("&type=$placeType")
-        googlePlacesUrl.append("&sensor=true")
-        googlePlacesUrl.append("&key=" + getString(R.string.google_maps_key))
-        Log.d("getUrl", googlePlacesUrl.toString())
-        return googlePlacesUrl.toString()
-    }
-
-    private fun getClient(baseUrl: String): Retrofit {
-        return Retrofit.Builder()
-                .baseUrl(baseUrl)
-                .addConverterFactory(GsonConverterFactory.create())
-                .build()
-    }
-
-    private fun addMarkerToMap(googlePlace: Results){
+    private fun addMarkerToMap(googlePlace: Results) {
         val lat = googlePlace.geometry!!.location!!.lat
         val lng = googlePlace.geometry!!.location!!.lng
         val location = LatLng(lat, lng)
@@ -105,24 +80,30 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
             MarkerOptions()
                 .position(location)
                 .title(googlePlace.name)
-                .icon(BitmapDescriptorService.bitmapFromVector(
-                applicationContext, R.drawable.ic_baseline_local_pizza_24)))
+                .snippet(googlePlace.rating.toString())
+                .icon(
+                    BitmapDescriptorService.bitmapFromVector(
+                        applicationContext, R.drawable.ic_baseline_local_pizza_24
+                    )
+                )
+        )
         pin.showInfoWindow()
     }
 
     private fun getNearByPlaceType(placeType: String) {
         val url = getUrl(this.latitude, this.longitude, placeType)
         mService.getNearbyPlaces(url!!)
-            .enqueue(object : Callback<Places>{
+            .enqueue(object : Callback<Places> {
                 override fun onResponse(call: Call<Places>, response: Response<Places>) {
-                    if (response.isSuccessful){
-                        for(i in response.body()!!.results!!.indices){
+                    if (response.isSuccessful) {
+                        for (i in response.body()!!.results!!.indices) {
                             addMarkerToMap(response.body()!!.results!![i])
                         }
                     }
                 }
+
                 override fun onFailure(call: Call<Places>, t: Throwable) {
-                    Toast.makeText(baseContext,""+t.message,Toast.LENGTH_SHORT).show()
+                    Toast.makeText(baseContext, "" + t.message, Toast.LENGTH_SHORT).show()
                 }
             })
     }
---
+++ app/src/main/java/com/styrala/findfood/common/Common.kt
@@ -0,0 +1,31 @@
+package com.styrala.findfood.common
+
+import android.util.Log
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object Common {
+    val MAPS_URL = "https://maps.googleapis.com/"
+    val API_KEY = "AIzaSyAo98VzOt144DOb5XuQMSRG1xuWDKWIzVs"
+    val RESTAURANT_TYPE = "restaurant"
+
+    fun getClient(baseUrl: String): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(baseUrl)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    fun getUrl(latitude: Double, longitude: Double, placeType: String): String? {
+        val googlePlacesUrl = StringBuilder(MAPS_URL)
+        googlePlacesUrl.append("maps/api/place/nearbysearch/json?")
+        googlePlacesUrl.append("location=$latitude,$longitude")
+        googlePlacesUrl.append("&radius=1000")
+        googlePlacesUrl.append("&type=$placeType")
+        googlePlacesUrl.append("&sensor=true")
+        googlePlacesUrl.append("&key=$API_KEY")
+        Log.d("URL: ", googlePlacesUrl.toString())
+        return googlePlacesUrl.toString()
+    }
+
+}
